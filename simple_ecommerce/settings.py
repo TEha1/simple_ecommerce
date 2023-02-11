6dbde8b18eb2639b12506d722b83c5725fb9acaa
Abdul-Fatah <abdelfatah.ramadan42@gmail.com>
2023-02-11 19:18:01 +0200

    Update settings.py

--- simple_ecommerce/settings.py
+++ simple_ecommerce/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",
-    "modeltranslation",
     # django default apps
     'django.contrib.admin',
     'django.contrib.auth',
